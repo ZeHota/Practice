5ceeede20b27cc463d3dcc24ac40c0882795b4fb
CJ <cjtakawalk@yahoo.com>
2020-07-30 16:12:06 -0700

    7/30/2020 objects

---
+++ pageObjects/yoodlizeObj.js
@@ -0,0 +1,184 @@
+module.exports = {
+    url: 'https://www.yoodlize.com',
+    elements: {
+        See1: {
+            selector: '(//div[text()="See all"])[1]',
+            locateStrategy: 'xpath'
+        },
+        See2: {
+            selector: '(//div[text()="See all"])[2]',
+            locateStrategy: 'xpath'
+        },
+        See3: {
+            selector: '(//div[text()="See all"])[3]',
+            locateStrategy: 'xpath'
+        },
+        See4: {
+            selector: '(//div[text()="See all"])[4]',
+            locateStrategy: 'xpath'
+        },
+        See5: {
+            selector: '(//div[text()="See all"])[5]',
+            locateStrategy: 'xpath'
+        },
+        See6: {
+            selector: '(//div[text()="See all"])[6]',
+            locateStrategy: 'xpath'
+        },
+        See7: {
+            selector: '(//div[text()="See all"])[7]',
+            locateStrategy: 'xpath'
+            
+        },
+        See8: {
+            selector: '(//div[text()="See all"])[8]',
+            locateStrategy: 'xpath'
+        },
+        See9: {
+            selector: '(//div[text()="See all"])[9]',
+            locateStrategy: 'xpath'
+        },
+        See10: {
+            selector: '(//div[text()="See all"])[10]',
+            locateStrategy: 'xpath'
+        },
+        See11: {
+            selector: '(//div[text()="See all"])[11]',
+            locateStrategy: 'xpath'
+        },
+        See12: {
+            selector: '(//div[text()="See all"])[12]',
+            locateStrategy: 'xpath'
+        },
+        See13: {
+            selector: '(//div[text()="See all"])[13]',
+            locateStrategy: 'xpath'
+        },
+        See14: {
+            selector: '(//div[text()="See all"])[14]',
+            locateStrategy: 'xpath'
+        },
+        See15: {
+            selector: '(//div[text()="See all"])[15]',
+            locateStrategy: 'xpath'
+            
+        },
+        REC:{
+            selector:'(//div[text()="RECREATIONAL VEHICLES"])[2]',
+            locateStrategy: 'xpath'
+        },
+        OUT:{
+            selector:'(//div[text()="OUTDOOR GEAR"])[2]',
+            locateStrategy: 'xpath'
+        },
+        ELE:{
+            selector:'(//div[text()="ELECTRONICS"])[2]',
+            locateStrategy: 'xpath'
+        },
+        PAR:{
+                selector:'(//div[text()="PARTY & WEDDING"])',
+                locateStrategy: 'xpath'
+        },
+        TOO:{
+            selector:'(//div[text()="TOOLS"])[2]',
+            locateStrategy: 'xpath'
+        },
+        CLO:{
+                selector:'(//div[text()="CLOTHING"])[2]',
+                locateStrategy: 'xpath'
+        },
+        HOM:{
+            selector:'(//div[text()="HOME & KITCHEN"])[2]',
+            locateStrategy: 'xpath'
+        },
+        TOY:{
+                selector:'(//div[text()="TOYS & GAMES"])[2]',
+                locateStrategy: 'xpath'
+        },
+        LAW:{
+            selector:'(//div[text()="LAWN & GARDEN"])[2]',
+            locateStrategy: 'xpath'
+        },
+        SPO:{
+                selector:'(//div[text()="SPORTING GOODS"])[2]',
+                locateStrategy: 'xpath'
+        },
+        DVD:{
+            selector:'(//div[text()="DVDS & VIDEO GAMES"])',
+            locateStrategy: 'xpath'
+        },
+        VEN:{
+                selector:'(//div[text()="VENUES"])[2]',
+                locateStrategy: 'xpath'
+        },
+        MUS:{
+            selector:'(//div[text()="MUSIC"])[2]',
+            locateStrategy: 'xpath'
+        },
+        MIS:{
+                selector:'(//div[text()="MISC"])[2]',
+                locateStrategy: 'xpath'
+        },
+        Rec: {
+            selector: '(//div[text()="category: Recreational Vehicles"])',
+            locateStrategy: 'xpath'
+        },
+        Out: {
+            selector: '(//div[text()="category: Outdoor Gear"])',
+            locateStrategy: 'xpath'
+        },
+        Ele: {
+            selector: '(//div[text()="category: Electronics"])',
+            locateStrategy: 'xpath'
+        },
+        Par: {
+            selector: '(//div[text()="category: Party & Wedding Equipment"])',
+            locateStrategy: 'xpath'
+        },
+        Too: {
+            selector: '(//div[text()="category: Tools"])',
+            locateStrategy: 'xpath'
+        },
+        Clo: {
+            selector: '(//div[text()="category: Clothing"])',
+            locateStrategy: 'xpath'
+        },
+        Hom: {
+            selector: '(//div[text()="category: Home and Kitchen"])',
+            locateStrategy: 'xpath'
+        },
+        Toy: {
+            selector: '(//div[text()="category: Toys and Games"])',
+            locateStrategy: 'xpath'
+        },
+        Law: {
+            selector: '(//div[text()="category: Lawn and Garden"])',
+            locateStrategy: 'xpath'
+        },
+        Spo: {
+            selector: '(//div[text()="category: Sporting Equipment"])',
+            locateStrategy: 'xpath'
+        },
+        Dvd: {
+            selector: '(//div[text()="category: DVDs"])',
+            locateStrategy: 'xpath'
+        },
+        Ven: {
+            selector: '(//div[text()="category: Venues"])',
+            locateStrategy: 'xpath'
+        },
+        Mus :{
+            selector: '(//div[text()="category: Music"])',
+            locateStrategy: 'xpath'
+        },
+        Bus: {
+            selector: '(//div[text()="category: Business Equipment"])',
+            locateStrategy: 'xpath'
+        },
+        Mis: {
+            selector: '(//div[text()="category: Misc"])',
+            locateStrategy: 'xpath'
+        }
+    }
+
+}
